eb1fa074be11dcfd526a130f9dcbc25767146b7d
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2021-03-14 16:15:34 -0700

    push update

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, send_from_directory
 from flask_session import Session
 # from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
